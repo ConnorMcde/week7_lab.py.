ab259902c477289f8fd0aba208b9c57d55358230
ConnorMcde <26596059@edgehill.ac.uk>
2025-11-21 14:52:39 +0000

    Update Week 7 diary Entry.py

--- Week 7 diary Entry.py
+++ Week 7 diary Entry.py
@@ -2,11 +2,12 @@
 
 diary_entry = input("Enter your diary entry: ")
 
-with open("my_diary.txt", "w") as f:
+with open("my_diary.txt", "a") as f:
  f.write(diary_entry + "\n")#\n adds new lines
 
 with open("my_diary.txt", "r") as f:
  file = f.read()
  
  print(f"Your diary entry was: {file}")
+
  
